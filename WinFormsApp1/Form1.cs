984f475fafd38313e5bb2b35871d703b6af23f71
sino <sino@TSC>
2024-10-22 22:19:20 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             int vizeNotu = Convert.ToInt32(textBox3.Text);
+            int finalNotu = Convert.ToInt32(textBox4.Text);
+            double ortalama = vizeNotu * 0.4 + finalNotu * 0.6;
+            {
+                listBox1.Items.Add(textBox1.Text + " " + textBox2.Text + " " + ortalama);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
